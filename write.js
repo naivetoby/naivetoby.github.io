05cd78818626c13c5e1fdfd2882954e9be2582b6
陶开杰 <naivetoby@gmail.com>
2025-07-10 21:02:31 +0800

    代码优化

--- write.js
+++ write.js
@@ -13,109 +13,107 @@
  * textIndex：当前所在文本的位置
  */
 function WRITE(obj) {
-    this.boxClass = obj.box
-    this.title = obj.title || null
-    this.signature = obj.signature || null
-    this.date = obj.date || null
-    this.text = obj.text || 'Hello World!'
-    this.textArray = this.text.split('\n')
-    this.paragraphLength = this.textArray.length
-    this.paragraphIndex = 0
-    this.textLength = this.textArray[this.paragraphIndex].length
-    this.textIndex = 0
-    this.isSignatureEnd = false
-    this.isDateEnd = false
-
-    var _this = this
-    if (this.title && this.title != '') {
-        this.newTitle()
-    } else {
-        this.newParagraph()
-    }
+  this.boxClass = obj.box
+  this.title = obj.title || null
+  this.signature = obj.signature || null
+  this.date = obj.date || null
+  this.text = obj.text || 'Hello World!'
+  this.textArray = this.text.split('\n')
+  this.paragraphLength = this.textArray.length
+  this.paragraphIndex = 0
+  this.textLength = this.textArray[this.paragraphIndex].length
+  this.textIndex = 0
+  this.isSignatureEnd = false
+  this.isDateEnd = false
+  if (this.title && this.title != '') {
+    this.newTitle()
+  } else {
+    this.newParagraph()
+  }
 }
 
 WRITE.prototype.newTitle = function () {
-    var h = '<p class="write-h1"><span class="cursor">&nbsp;&nbsp;&nbsp;</span></p>'
-    $(this.boxClass).append(h)
-    $('.text-box>p:last-child')[0].scrollIntoView();
-    var timer = window.setTimeout(function () {
-        this.addText(this.title)
-    }.bind(this), 1000)
+  var h = '<p class="write-h1"><span class="cursor">&nbsp;&nbsp;&nbsp;</span></p>'
+  $(this.boxClass).append(h)
+  $('.text-box>p:last-child')[0].scrollIntoView();
+  var timer = window.setTimeout(function () {
+    this.addText(this.title)
+  }.bind(this), 1000)
 }
 
 WRITE.prototype.newParagraph = function () {
-    var p = '<p class="write-p" data-index="' + this.paragraphIndex + '"><span class="cursor">&nbsp;&nbsp;&nbsp;</span></p>'
-    $(this.boxClass).append(p)
-    $('.text-box>p:last-child')[0].scrollIntoView();
-    var timer = window.setTimeout(function () {
-        this.addParagraph()
-    }.bind(this), (this.isSignatureEnd || this.isDateEnd) ? 0 : 3000)
+  var p = '<p class="write-p" data-index="' + this.paragraphIndex + '"><span class="cursor">&nbsp;&nbsp;&nbsp;</span></p>'
+  $(this.boxClass).append(p)
+  $('.text-box>p:last-child')[0].scrollIntoView();
+  var timer = window.setTimeout(function () {
+    this.addParagraph()
+  }.bind(this), (this.isSignatureEnd || this.isDateEnd) ? 0 : 3000)
 }
 
 WRITE.prototype.newSignature = function () {
-    if (this.isSignatureEnd === false) {
-        var p = '<p class="write-signature"><span class="cursor">&nbsp;&nbsp;&nbsp;</span></p>'
-        $(this.boxClass).append(p)
-        $('.text-box>p:last-child')[0].scrollIntoView();
-        var timer = window.setTimeout(function () {
-            this.addText(this.signature)
-            this.isSignatureEnd = true
-        }.bind(this), 0)
-    } else if (this.isDateEnd === false) {
-        var p = '<p class="write-signature"><span class="cursor">&nbsp;&nbsp;&nbsp;</span></p>'
-        $(this.boxClass).append(p)
-        $('.text-box>p:last-child')[0].scrollIntoView();
-        var timer = window.setTimeout(function () {
-            this.addText(this.date)
-            this.isDateEnd = true
-        }.bind(this), 0)
-    }
+  if (this.isSignatureEnd === false) {
+    var p = '<p class="write-signature"><span class="cursor">&nbsp;&nbsp;&nbsp;</span></p>'
+    $(this.boxClass).append(p)
+    $('.text-box>p:last-child')[0].scrollIntoView();
+    var timer = window.setTimeout(function () {
+      this.addText(this.signature)
+      this.isSignatureEnd = true
+    }.bind(this), 0)
+  } else if (this.isDateEnd === false) {
+    var p = '<p class="write-signature"><span class="cursor">&nbsp;&nbsp;&nbsp;</span></p>'
+    $(this.boxClass).append(p)
+    $('.text-box>p:last-child')[0].scrollIntoView();
+    var timer = window.setTimeout(function () {
+      this.addText(this.date)
+      this.isDateEnd = true
+    }.bind(this), 0)
+  }
 }
 
 WRITE.prototype.addParagraph = function () {
-    if (this.textArray[this.paragraphIndex]) {
-        this.addText(this.textArray[this.paragraphIndex])
-    } else if (this.signature) {
-        $('.cursor').remove()
-        this.newSignature()
-    }
+  if (this.textArray[this.paragraphIndex]) {
+    this.addText(this.textArray[this.paragraphIndex])
+  } else if (this.signature) {
+    $('.cursor').remove()
+    this.newSignature()
+  }
 }
 
 WRITE.prototype.addText = function (text) {
-    if (text && text.length > 0) {
-        var _this = this
-        var $cursor = $('.cursor')
-        var length = text.length
-        var timer2 = null
-        var audio = $('audio')[0]
-        var timer = window.setInterval(function () {
-            if (_this.textIndex < length && (text[_this.textIndex] == ',' || text[_this.textIndex] == '，' || text[_this.textIndex] == '.') || text[_this.textIndex] == '。') {
-                audio.pause()
-                clearInterval(timer)
-                timer = null
-                $cursor.before(text[_this.textIndex])
-                $('.text-box>p:last-child')[0].scrollIntoView();
-                _this.textIndex++
-                timer2 = setTimeout(function () {
-                    _this.addText(text)
-                }, 1300)
-            } else if (_this.textIndex < length) {
-                audio.play()
-                $cursor.before(text[_this.textIndex])
-                $('.text-box>p:last-child')[0].scrollIntoView();
-                _this.textIndex++
-            } else {
-                audio.pause()
-                clearInterval(timer)
-                timer = null
-                _this.paragraphIndex++
-                _this.textIndex = 0
-                $cursor.remove()
-                $('.text-box>p:last-child')[0].scrollIntoView();
-                _this.newParagraph()
-            }
-        }, 150)
-    } else {
+  if (text && text.length > 0) {
+    var _this = this
+    var $cursor = $('.cursor')
+    var length = text.length
+    var timer2 = null
+    var audio = $('audio')[0]
+    var timer = window.setInterval(function () {
+      if (_this.textIndex < length && (text[_this.textIndex] == ',' || text[_this.textIndex] == '，' || text[_this.textIndex] == '.') || text[_this.textIndex] == '。') {
+        audio.pause()
+        clearInterval(timer)
+        timer = null
+        $cursor.before(text[_this.textIndex])
+        $('.text-box>p:last-child')[0].scrollIntoView();
+        _this.textIndex++
+        timer2 = setTimeout(function () {
+          _this.addText(text)
+        }, 1300)
+      } else if (_this.textIndex < length) {
+        audio.play()
+        $cursor.before(text[_this.textIndex])
+        $('.text-box>p:last-child')[0].scrollIntoView();
+        _this.textIndex++
+      } else {
+        audio.pause()
+        clearInterval(timer)
+        timer = null
+        _this.paragraphIndex++
+        _this.textIndex = 0
+        $cursor.remove()
+        $('.text-box>p:last-child')[0].scrollIntoView();
+        _this.newParagraph()
+      }
+    }, 150)
+  } else {
 
-    }
+  }
 }
